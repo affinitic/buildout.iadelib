cc35152baabf8ceb12f780cee6da7781ee4846ae
Martin Peeters <martin.peeters@affinitic.be>
2021-03-16 15:57:47 +0100

    Cleanup

--- scripts/remove_contents.py
+++ scripts/remove_contents.py
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
-import argparse
 
 from Zope2.App import startup
 from plone import api
 from transaction import commit
 from zope.component.hooks import setSite
 
+import argparse
+
 
 def remove_contents():
-    portal = api.portal.get()
-    meeting_configs = api.content.find(portal_type='MeetingConfig')
+    meeting_configs = api.content.find(portal_type="MeetingConfig")
     for meeting_config in meeting_configs:
         config = meeting_config.getObject()
 
@@ -25,21 +25,18 @@ def remove_contents():
         meeting_items = api.content.find(portal_type=meeting_item_name)
         for meeting_item in meeting_items:
             api.content.delete(meeting_item.getObject())
-            # portal.restrictedTraverse('@@delete_givenuid')(meeting_item.UID)
 
         meeting_name = "Meeting{0}".format(config.shortName)
         meetings = api.content.find(portal_type=meeting_name)
         for meeting in meetings:
             api.content.delete(meeting.getObject())
-            # portal.restrictedTraverse('@@delete_givenuid')(meeting.UID)
 
 
 def main(app):
     startup.startup()
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-c')
-    parser.add_argument('name', help='Name of plone site', default='Plone')
+    parser.add_argument("name", help="Name of plone site", default="Plone")
     args = parser.parse_args()
 
     setSite(app[args.name])
